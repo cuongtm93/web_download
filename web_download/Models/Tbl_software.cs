deda4d134ff69ec7d6c55452804ea57179511f77
cuongtm93 <developer.tranmanhcuong@gmail.com>
2020-01-05 08:03:43 +0700

    create

---
+++ web_download/Models/Tbl_software.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_download.Models
+{
+    public class Tbl_software
+    {
+
+    }
+}
---
+++ web_download/Models/WebDownloadDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_download.Models
+{
+    public class WebDownloadDbContext : DbContext
+    {
+         
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(Startup.Configuration["ConnectionString"]);
+            Database.EnsureCreated();
+        }
+    }
+}
