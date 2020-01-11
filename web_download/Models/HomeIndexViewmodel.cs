a6f1b3e04adfd313d279828f261dfc88c10333a6
cuongtm93 <39010685+cuongtm93@users.noreply.github.com>
2020-01-12 04:22:02 +0700

    cập nhật fontawsome;

---
+++ web_download/Models/HomeIndexViewmodel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webdownload.Models
+{
+    public class HomeIndexViewmodel
+    {
+        public List<TblSoftware> News_Softwares { get; set; }
+        public List<TblSoftware> Top_Softwares { get; set; }
+    }
+}
